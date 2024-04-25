75ee28212032e5838cbb423c802d6ef722481d1d
mxolisi022 <mxolisingobese2@gmail.com>
2024-04-25 13:01:39 +0200

    banking commit

---
+++ ATM.java
@@ -0,0 +1,11 @@
+
+import java.io.IOException;
+
+public class ATM extends Option {
+    public static void main(String[] arg) throws IOException {
+        Option option = new Option();
+
+        option.getLogin();
+    }
+}
+
